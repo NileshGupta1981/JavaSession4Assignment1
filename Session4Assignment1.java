4660a68a7cbd64e4448139cf20c8db6dfff5b181
NileshGupta1981 <nileshgupta1981@gmail.com>
2016-04-26 21:47:37 +0530

    Session4Assignment1

---
+++ Session4Assignment1.java
@@ -0,0 +1,19 @@
+public class Session4Assignment1{
+	
+	public static void main (String args[]){
+		int[] a = new int[5];
+		 a[0]= 1;
+		 a[1]=2;
+		 a[2]=3;
+		 a[3]=4;
+		 a[4]=5;
+		 
+		int i;
+		System.out.println ("Arrays values in reverse are");
+		 for ( i=a.length-1 ; i > 0 ; i--)
+		 {
+			 System.out.println(a[i]);
+		 }	 
+		
+	}
+}
